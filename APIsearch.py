c58dd663829a8bc7db891af42e3bdb00223aee9a
liao <liao961120@gmail.com>
2020-11-29 19:33:58 +0800

    first commit

---
+++ APIsearch.py
@@ -0,0 +1,82 @@
+import json
+import requests
+
+
+def search(pattern=None, cql_enable=True, board=None, year_from=None, year_to=None, author=None, number=20, wordsaroundhit=0):
+    PTT_BLACKLAB_API = "http://140.112.147.132:8999/blacklab-server"
+    
+    filters = []
+    filter_string = ""
+
+    if pattern is None and isinstance(pattern, str): 
+        raise ValueError("pattern must be given in type <string>!")
+
+    if type(year_from) != type(year_to):
+        raise ValueError("year_from and year_to must be in type <string>!")
+
+    if not cql_enable:
+        pattern = f"[word=\"{pattern}\"]"
+
+    if board is not None:
+        filters.append(f'board:("{board}")')
+
+    if year_from is not None and year_to is not None:
+        filters.append(f'year:[{year_from} TO {year_to}]')
+
+    if len(filters) != 0:
+        filter_string = " AND ".join(filters) + "&"
+    
+    # Pagination
+    num_per_page = 3000
+    num_of_pages = int(number // num_per_page + 0.01) + 1
+    params = {
+            'outputformat': 'json',
+            'filter': filter_string,
+            'waitfortotal': 'yes',
+            'wordsaroundhit': wordsaroundhit,
+            'patt': pattern,
+            'first': 0,
+            'number': num_per_page
+        }
+
+    hits = []
+    for i in range(num_of_pages):
+        # Last page
+        if i == num_of_pages - 1:
+            params['number'] = number % num_per_page
+        
+        response = requests.get(f'{PTT_BLACKLAB_API}/indexes/hits', params=params)
+        print(response.url)
+        text = json.loads(response.text)
+        if i == 0:
+            num_of_results_found = text.get("summary").get("numberOfHits")
+            print(f'  Found {num_of_results_found} results')
+        if num_of_results_found <= len(hits): break
+        hits += text.get("hits")
+        params['first'] += num_per_page
+
+    return hits
+
+
+def get_capture_groups(hit, pos_tags=False):
+    s_idx = hit['start']
+    
+    fullcntx_words = hit['left']['word'] + hit['match']['word'] + hit['right']['word']
+    fullcntx_tags = hit['left']['pos'] + hit['match']['pos'] + hit['right']['pos']
+    
+    groups = {}
+    for g in hit['captureGroups']:
+        start, end = g['start'] - s_idx, g['end'] - s_idx
+        
+        tokens = fullcntx_words[start:end]
+        if pos_tags:
+            tags = fullcntx_tags[start:end]
+            tokens = [(tokens[i], tags[i]) for i in range(len(tokens))]
+        
+        groups[g['name']] = tokens
+    
+    return groups
+
+
+def top_n(freq_table: dict, n=25):
+    return sorted(freq_table.items(), key=lambda x: x[1], reverse=True)[:n]
---
+++ collo_measures.py
@@ -0,0 +1,141 @@
+from math import log2, log, sqrt
+
+def cca(freq_table):
+    """
+    Covarying Collexeme Analysis
+    freq_table: dict. Format: {(Slot1, Slot2): freq, (Slot1, Slot2): freq, ...}
+    """
+
+    # Get total size
+    corp_size = sum(v for k, v in freq_table.items())
+    
+    # Get marginal sizes
+    subj_totals = {}
+    act_totals = {}
+    for k, v in freq_table.items():
+        subj, act = k
+        if subj not in subj_totals: subj_totals[subj] = 0
+        if act not in act_totals: act_totals[act] = 0
+        subj_totals[subj] += v
+        act_totals[act] += v
+    
+    # Compute collocation scores
+    new_freq_table = {}
+    for k, v in freq_table.items():
+        subj, act = k
+        subj_total, act_total = subj_totals[subj], act_totals[act]
+        othersubj_total, otheract_total = corp_size - subj_total, corp_size - act_total
+        
+        # Compute observed frequencies
+        o11 = v
+        o12 = subj_total - o11
+        o21 = act_total - o11
+        o22 = othersubj_total - o21
+
+        if o11 < 0 or o12 < 0 or o21 < 0 or o22 < 0:
+            print(k)
+            print(o11, o12, o21, o22)
+            print(subj_total, act_total)
+            raise Exception('negative freq')
+
+        # Compute expected frequencies
+        contingency_table = {
+            'o11': o11,
+            'o12': o12,
+            'o21': o21,
+            'o22': o22
+        }
+        new_freq_table[k] = measures(**contingency_table)
+    
+    return new_freq_table
+
+
+
+def dca(freq_table):
+    """
+    Distinctive Collexeme Analysis
+    freq_table: dict. Format: {C1: {L1: freq, L2: freq, ...}, C2: {L1: freq, L2: freq, ...}}
+    
+         Lj     ~Lj
+    C1   o11    o12
+    C2   o21    o22
+    """
+    cnst_keys = list(freq_table.keys())
+    C1 = cnst_keys[0]
+    C2 = cnst_keys[1]
+
+    words = set()
+    freq_totals = {}
+    for key in cnst_keys:
+        freq_totals[key] = 0
+        for w in freq_table[key]:
+            words.add(w)
+            freq_totals[key] += freq_table[key][w]
+    
+    # Compute collocation measures
+    scores = {}
+    for w in words:
+        a = freq_table[C1].get(w, 0)
+        b = freq_totals[C1] - a
+        c = freq_table[C2].get(w, 0)
+        d = freq_totals[C2] - c
+        try:
+            scores[w] = measures(o11=a, o12=b, o21=c, o22=d)
+        except:
+            print(a, b, c, d)
+    
+    print(f'Pos: attract to {C1}\nNeg: attract to {C2}')
+    return scores #{'scores': scores, 'pos': C1}
+
+
+
+def measures(o11, o12, o21, o22):
+    """
+    G2: 3.8415 (p < 0.05); 10.8276 (p < 0.01)
+    """
+
+    o1_ = o11 + o12
+    o2_ = o21 + o22
+    o_1 = o11 + o21
+    o_2 = o12 + o22
+
+    total = o1_ + o2_
+    e11 = o1_ * o_1 / total
+    e12 = o1_ * o_2 / total
+    e21 = o2_ * o_1 / total
+    e22 = o2_ * o_2 / total
+    
+    try:
+        t11 = o11*log(o11/e11) if o11 != 0 else 0
+        t12 = o12*log(o12/e12) if o12 != 0 else 0
+        t21 = o21*log(o21/e21) if o21 != 0 else 0
+        t22 = o22*log(o22/e22) if o22 != 0 else 0
+    except:
+        print(o11, o12, o21, o22, e11, e12, e21, e22)
+        raise Exception('math error')
+    G2 = 2 * (t11 + t12 + t21 + t22)
+    if o11 < e11: G2 = -G2
+
+    if o11 == 0: o11 = 0.00000000001
+    return {
+        'freq': o11,
+        "G2": G2,
+        'MI': log2(o11/e11),
+        'MI3': log2(o11 ** 3 / e11),
+        'MI_logf': log2(o11/e11) * log(o11 + 1),
+        't': (o11 - e11) / sqrt(e11),
+        'Dice': 2 * e11 / (o1_ + o_1),
+        "deltaP21": o11/o1_ - o21/o2_,
+        "deltaP12": o11/o_1 - o21/o_2
+    }
+
+
+def rank_collo(collo_measures, nodeword=None, nodeword_idx=0, sort_by='G2', reverse=True, freq_cutoff=None):
+    if freq_cutoff is None: freq_cutoff = 0
+    out = sorted( ((k, v[sort_by], v['freq']) for k, v in collo_measures.items() if v['freq'] >= freq_cutoff),
+    key=lambda x: x[1], reverse=reverse)
+
+    if nodeword:
+        return [ x for x in out if x[0][nodeword_idx] == nodeword.strip() ]
+    else:
+        return out
